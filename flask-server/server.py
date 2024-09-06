5e0389f85825cf127e0ac7e1953e11a33034f845
Aniket Gupta <ganiketwork@gamil.com>
2024-09-07 01:33:26 +0530

    updated server file along with database

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_swagger_ui import get_swaggerui_blueprint
-
-app = Flask(__name__)
+from config import app, db
+from models import Contact
 
 SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
 API_URL = '/static/openapi.json'  # Our API url (can of course be a local resource)
@@ -28,13 +28,74 @@ app.register_blueprint(swaggerui_blueprint)
 
 # Now point your browser to localhost:5000/api/docs/
 
+
+# Contact API
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})
+
+
+@app.route("/create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+
+    if not first_name or not last_name or not email:
+        return (
+            jsonify({"message": "You must include a first name, last name and email"}),
+            400,
+        )
+
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+
+    return jsonify({"message": "User created!"}), 201
+
+
+@app.route("/update_contact/<int:user_id>", methods=["PATCH"])
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+
+    return jsonify({"message": "User updated."}), 200
+
+
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
+def delete_contact(user_id):
+    contact = Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+
+    db.session.delete(contact)
+    db.session.commit()
+
+    return jsonify({"message": "User deleted!"}), 200
+
 # Member API
 
-@app.route('/members')
+@app.route("/members", methods=["GET"])
 def member():
     return {"members": ["Member 1", "Member 2"]}
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
-
-
